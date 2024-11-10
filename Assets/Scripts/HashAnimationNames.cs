74075dd8c2cc543ad2b4839c711133c8d3a1b200
Vis <Vish106@rambler.ru>
2024-11-10 13:17:20 +0300

    1 attempt

---
+++ Assets/Scripts/HashAnimationNames.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class HashAnimationNames
+{
+    public static class PlayerAnimation
+    {
+        public static int SpeedHash = Animator.StringToHash("Speed");
+        public static int IsGroundedHash = Animator.StringToHash("IsGrounded");
+    }    
+}
